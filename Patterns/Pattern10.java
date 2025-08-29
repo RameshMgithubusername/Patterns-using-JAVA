6070ab08a4f23a0274a082a7ebbf0243aa21e5e9
RameshMgithubusername <160245138+RameshMgithubusername@users.noreply.github.com>
2025-08-30 05:21:11 +0530

    Update Pattern10.java

--- Patterns/Pattern10.java
+++ Patterns/Pattern10.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Pattern10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Diamond Half Pattern");
         System.out.print("Enter the number: ");
         int x = sc.nextInt();
         pattern(x);
