da8109034eb355fdad9deb961789bb901072b779
RameshMgithubusername <160245138+RameshMgithubusername@users.noreply.github.com>
2025-08-30 05:46:38 +0530

    Update Pattern19.java

--- Patterns/Pattern19.java
+++ Patterns/Pattern19.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Pattern19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Butterfly Pattern");
         System.out.print("Enter the number: ");
         int x = sc.nextInt();
         pattern(x);
