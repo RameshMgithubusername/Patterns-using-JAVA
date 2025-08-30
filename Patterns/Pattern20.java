3458eaa5230fd24cc3080f1262d3b7c480adc0ea
RameshMgithubusername <160245138+RameshMgithubusername@users.noreply.github.com>
2025-08-30 06:00:29 +0530

    Update Pattern20.java

--- Patterns/Pattern20.java
+++ Patterns/Pattern20.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Pattern20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.pritnln("Hollow Hourglass Star Pattern");
         System.out.print("Enter the number: ");
         int x = sc.nextInt();
         pattern(x);
