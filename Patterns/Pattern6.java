a8fe2a6b766200182db7213988552175da9c60a4
RameshMgithubusername <160245138+RameshMgithubusername@users.noreply.github.com>
2025-08-30 06:14:02 +0530

    Update Pattern6.java

--- Patterns/Pattern6.java
+++ Patterns/Pattern6.java
@@ -4,6 +4,7 @@ public class Pattern6 {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Inverted Right-Angle Reverse Order");
         System.out.print("Enter number:");
         int x = sc.nextInt();
         pattern1(x);
